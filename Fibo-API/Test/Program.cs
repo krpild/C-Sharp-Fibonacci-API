eb4ef6f20a7fea89d9f323301ee552259c886e26
krpild <kristjan.pilden@gmail.com>
2025-04-04 00:25:28 +0300

    Matrix exponentiation Fibonacci implemented

---
+++ Fibo-API/Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using Fibonacci;
+
+namespace Test;
+
+public class Test
+{
+    static void Main(String[] args)
+    {
+        
+        FibonacciCalculation fibo = new FibonacciCalculation();
+        
+        for (int i = 0; i <= 100; i++)
+        {
+            Console.WriteLine(fibo.Fibonacci(i));
+        }
+    } 
+}
